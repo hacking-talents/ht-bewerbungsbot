b2bcbb9646e8c74b053f8d5ab171200531524e3e
Robert Deppe <deppe@sipgate.de>
2021-07-09 09:42:15 +0200

    :recycle: refine errormojis

--- src/errormojis.ts
+++ src/errormojis.ts
@@ -1,9 +1,9 @@
 export enum EmojiErrorCodes {
   UNEXPECTED_HTTP = "☄️", // "I did not expect this cathastrophe"
   UNEXPECTED = "🤷", // "Dunno what dis?"
-  USER_NOT_FOUND = "🤔",
+  USER_NOT_FOUND = "🤔", // "I don't know that guy"
   PROJECT_NOT_FOUND = "📂", // Empty folder has no projects in it
   FORK_FAILED = "🍴", // A literal fork
-  MISSING_CANDIDATE_FIELD = "🗒️",
-  PIPELINE_STAGE_NOT_FOUND = "🗃️",
+  MISSING_CANDIDATE_FIELD = "🌾", // "No field as far as the eye can see"
+  PIPELINE_STAGE_NOT_FOUND = "🎤", // "Would you please come on stage?"
 }
